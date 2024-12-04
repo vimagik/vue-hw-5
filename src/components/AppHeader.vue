ad1e140c5219e12bc220e43d3bd56bb094e8e1ac
George Rzhavin <g.rzhavin@yandex.ru>
2024-12-04 22:43:12 +0300

    Created dropdown menu with logout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,12 +12,13 @@ const userStore = useUserStore()
 
 <template>
     <v-app-bar>
-        <v-app-bar-title>Супер-пупер магазин {{ userStore.username }}</v-app-bar-title>
+        <v-app-bar-title>Супер-пупер магазин</v-app-bar-title>
         <v-spacer></v-spacer>
         <v-text-field v-if="route.path === '/'" class="mt-4 mr-4" prepend-icon="mdi-magnify" density="compact"
             variant="outlined" max-width="300" v-model="searchStr"></v-text-field>
         <v-btn to="/">Главная</v-btn>
-        <v-btn class="ml-2" to="/newproduct">Добавить продукт</v-btn>
+        <v-btn v-if="userStore.username === ''" class="ml-2" to="/login">Войти</v-btn>
+        <app-menu class="ml-3" v-else />
         <v-badge v-if="route.path === '/'" :content="store.productAmmount" :dot="store.productAmmount === 0" inline>
             <v-btn color="indigo" icon="mdi-cart-variant" to="/cart"></v-btn>
         </v-badge>
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { useRouter } from "vue-router";
 
 
@@ -9,13 +9,18 @@ export const useUserStore = defineStore('user', () => {
     
     function logout() {
         username.value = ''
+        localStorage.setItem("user", '')
     }
 
     function login(name) {
         username.value = name
         localStorage.setItem("user", name)
         router.push('/')
-     }
+    }
 
-    return { username, logout, login }
+    const acronym = computed(() => {
+        return username.value.slice(0, 2)
+    })
+
+    return { username, logout, login, acronym}
 })
